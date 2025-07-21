f245c212120b3a394ba47d202389f6f48dad5a9d
Thibault Gouchon <thibaultgouchon@gmail.com>
2025-07-21 09:58:16 +0200

    Second room

---
+++ Assets/Script/EscapeManager.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscapeManager : MonoBehaviour
+{
+    [Header("Séquence attendue")]
+    public List<int> correctSequence = new List<int> { 1, 2, 3 };
+
+    [Header("Objets")]
+    public GameObject door;
+    public Animator ceilingAnimator;
+
+    private List<int> currentSequence = new List<int>();
+    private int currentError = 0;
+
+    public void OnPlatePressed(int index)
+    {
+        currentSequence.Add(index);
+
+        if (!IsSequenceCorrectSoFar())
+        {
+            TriggerFallAnimation();
+        }
+        else if (currentSequence.Count == correctSequence.Count)
+        {
+            OpenDoor();
+        }
+    }
+
+    bool IsSequenceCorrectSoFar()
+    {
+        for (int i = 0; i < currentSequence.Count; i++)
+        {
+            if (currentSequence[i] != correctSequence[i])
+                return false;
+        }
+        return true;
+    }
+
+    void TriggerFallAnimation()
+    {
+        if (currentError >= 4)
+        {
+            Debug.Log("Game over !");
+            return;
+        }
+
+        ceilingAnimator.SetTrigger("fall_" + (currentError + 1));
+        currentError++;
+    }
+
+    void OpenDoor()
+    {
+        door.SetActive(false);
+    }
+}
